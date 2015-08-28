87794b41d5d843b875d73350c8340dd64235926c
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-08-28 22:20:58 +0100

    dep update

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject clj-nats-async "0.4.1"
+(defproject clj-nats-async "0.4.2"
   :description "an async client for NATS, wrapping java-nats"
   :url "https://github.com/employeerepublic/clj-nats-async"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
 
   :dependencies [[com.github.cloudfoundry-community/nats-client "0.6.3"]
-                 [manifold "0.1.1-alpha3"]]
+                 [manifold "0.1.1-alpha4"]]
 
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.7.0"]]}})

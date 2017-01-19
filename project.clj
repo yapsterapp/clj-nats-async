fa0cdf56d8f83197e5eb85d749041296bd12952a
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2017-01-19 23:07:09 +0000

    update jnats dep

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject clj-nats-async "1.0.0"
+(defproject clj-nats-async "1.1.0"
   :description "an async client for NATS, wrapping java-nats"
   :url "https://github.com/employeerepublic/clj-nats-async"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
 
-  :dependencies [[employeerepublic/jnats "0.4.1-2"]
+  :dependencies [[io.nats/jnats "0.7.3"]
                  [manifold "0.1.5"]]
 
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.8.0"]]}})
